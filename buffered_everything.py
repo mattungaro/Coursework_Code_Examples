d683f8455433b917c7ea37b79bda5bf79b14b371
mattungaro <74844849+mattungaro@users.noreply.github.com>
2021-10-22 14:09:49 -0400

    Update buffered_everything.py

--- buffered_everything.py
+++ buffered_everything.py
@@ -4,7 +4,8 @@ Created on Thu Oct 22 18:41:01 2020
 
 @author: Owner
 """
-
+# all census areas are buffered by 1 mile, as I made an assumption that people could walk up to 1 mile from their home to a park 
+# or some other green space where a river/stream may be located.
 
 import arcpy
 arcpy.env.workspace = r"C:\Users\Owner\Documents\UNC_SPRING2021\Adv_GIS\demographics"
